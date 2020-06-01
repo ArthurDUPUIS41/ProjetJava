06d1e78cb9343c893bf18e45b3bc3001debc485d
ROG <ROG@ROG>
2020-06-01 22:36:57 +0200

    Class ActionEffacer : fini

--- src/fr/eseo/poo/projet/artiste/controleur/actions/ActionEffacer.java
+++ src/fr/eseo/poo/projet/artiste/controleur/actions/ActionEffacer.java
@@ -8,17 +8,23 @@ import fr.eseo.poo.projet.artiste.vue.ihm.PanneauDessin;
 
 public class ActionEffacer extends AbstractAction{
 	
-	public static final String NOM_ACTION = null;
+	private static final long serialVersionUID = 1L;
+	public static final String NOM_ACTION = "Effacer dessin";
+	private PanneauDessin panneauMain;
 	
 	public ActionEffacer(PanneauDessin panneauDessin){	
 		super(NOM_ACTION);
+		this.panneauMain = panneauDessin;
 	}
 	
 	public void actionPerformed(ActionEvent event) {
-		final  optionPane = new ("Voulez vous tout effacer ?\n", NOM_ACTION, 
-			    										JOptionPane.QUESTION_MESSAGE,
-		    											JOptionPane.YES_NO_OPTION);	
+		int optionPane = JOptionPane.showConfirmDialog(panneauMain, "Voulez vous tout effacer ?\n", NOM_ACTION, JOptionPane.YES_NO_OPTION);
 		
+		//Si l'utilisateur appuie sur le bouton OUI 
+		if(optionPane == 0) {						
+			panneauMain.getVueFormes().clear();
+			this.panneauMain.repaint();
+		}		
 	}
 	
 	
--- src/fr/eseo/poo/projet/artiste/vue/ihm/PanneauBarreOutils.java
+++ src/fr/eseo/poo/projet/artiste/vue/ihm/PanneauBarreOutils.java
@@ -1,14 +1,15 @@
 package fr.eseo.poo.projet.artiste.vue.ihm;
 
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+
 
 public class PanneauBarreOutils extends JPanel{
 
 	private PanneauDessin panneauDessin;
 	
 	public PanneauBarreOutils(PanneauDessin panneauDessin) {
-		
-		
+		initComponents();
 	}
 	
 	public void initComponents(){
